02cb4b71698c4147911e377c3739ce7a7ba78ea8
Woodshox <1aleksandrpavlov@gmail.com>
2021-05-17 12:39:54 +0200

    INIT the project

---
+++ VK Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK Client
+//
+//  Created by Woodshox on 17.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
